3b84637f47c8ff8fb2808d6395fdd84b1dda6fb4
Steve BERENGER <steve.berenger@gmail.com>
2022-04-12 15:23:20 +0200

    first commit

---
+++ eclipse-workspace/p25/restcountries-api-1/src/main/java/co/simplon/restcountries/RestcountriesApi1Application.java
@@ -0,0 +1,13 @@
+package co.simplon.restcountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcountriesApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestcountriesApi1Application.class, args);
+	}
+
+}
