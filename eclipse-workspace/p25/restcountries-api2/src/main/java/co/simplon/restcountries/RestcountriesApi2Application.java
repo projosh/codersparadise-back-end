db026d75b4a9d680f47e44c59e7df16b0ab37591
Steve BERENGER <steve.berenger@gmail.com>
2022-04-13 11:26:58 +0200

    test branch

---
+++ eclipse-workspace/p25/restcountries-api2/src/main/java/co/simplon/restcountries/RestcountriesApi2Application.java
@@ -0,0 +1,13 @@
+package co.simplon.restcountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcountriesApi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestcountriesApi2Application.class, args);
+	}
+
+}
